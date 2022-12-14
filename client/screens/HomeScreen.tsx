0ae35f9363656bd4291ff45bed03022dd5a46a33
Akintunde Owojori <105000665+oneboyfromife@users.noreply.github.com>
2022-12-14 15:03:47 -0800

    Add files via upload

---
+++ client/screens/HomeScreen.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, SafeAreaView, Text } from "react-native";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  return (
+    <SafeAreaView>
+      <Text
+      // style={{
+      //   color: "red",
+      // }}
+      >
+        Onboarding Screen
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
