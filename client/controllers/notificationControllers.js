fefe3f3e5ae2609722cce5a103520f2e6731ef88
Leksyking <gbemilekeogundipe@gmail.com>
2023-01-19 20:22:17 +0100

    add get single, all and delete notifications

--- client/controllers/notificationControllers.js
+++ client/controllers/notificationControllers.js
@@ -1,5 +1,6 @@
 const Order = require("../models/order")
 const User = require("../models/user")
+const custom = require("../errors")
 const Notification = require("../models/notifications")
 
 
@@ -16,15 +17,23 @@ const createBoolNotification = async (req, res) => {
 }
 
 const getAllNotifications  = async (req, res) => {
-    
+    const notif = await Notification.find({user: req.user.userId});
+    res.status(200).json({notificatons: notif})
 }
 
 const getSingleNotification = async (req, res) => {
-    
+    const {id: notificationId} = req.params;
+    const notif = await Notification.findById(notificationId);
+    if (!notif){
+        throw new custom.NotFoundError(`No Notification with id: ${notificationId}`)
+    }
+    res.status(200).json({notificaton: notif})
 }
 
 const deleteNotification = async (req, res) => {
-    
+    const {id: notificationId} = req.params;
+    await Notification.findByIdAndDelete(notificationId);
+    res.status(200).json({msg: "Success"})
 }
 
 module.exports = {
